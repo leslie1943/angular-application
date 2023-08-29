47a9e3eb9d18e5a109cb3426f5427370587d08d3
Leslie Su <leslie43@sina.com>
2023-08-29 21:37:02 +0800

    form builder

--- src/app/components/reactive-form-build/reactive-form-build.component.ts
+++ src/app/components/reactive-form-build/reactive-form-build.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-build',
   templateUrl: './reactive-form-build.component.html',
   styleUrls: ['./reactive-form-build.component.scss'],
 })
-export class ReactiveFormBuildComponent {}
+export class ReactiveFormBuildComponent {
+  contactForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      fullName: this.fb.group({
+        firstName: this.fb.control('', [Validators.required]), // 也可以直接写 firstName: ['', Validators.required]
+        lastName: ['', Validators.required],
+      }),
+      phone: [],
+      contacts: this.fb.array([]),
+    });
+  }
+
+  onSubmit() {
+    // 仅仅使用 get 获取的是 FormGroup / FormArray / FormControl
+    console.info('phone', this.contactForm.get('phone')); // FormControl
+    console.info('contacts', this.contactForm.get('contacts')); // FormArray
+    console.info('fullName', this.contactForm.get('fullName')); // FormGroup
+
+    // 使用value获取的才是真正的value
+    console.info('fullName', this.contactForm.get('fullName')?.value);
+    console.info(
+      'firstName',
+      this.contactForm.get('fullName')?.get('firstName')?.value
+    );
+    console.info(
+      'lastName',
+      this.contactForm.get('fullName')?.get('lastName')?.value
+    );
+    console.info(this.contactForm.value);
+  }
+}
